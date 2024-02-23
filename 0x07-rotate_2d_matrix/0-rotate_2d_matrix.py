fc9807e85ec9444261097132481a4f1854feaf0b
sarahassan0 <sarraree5@gmail.com>
2024-02-23 04:03:05 +0200

    feat: implement `rotate_2d_matrix` function

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" rotate_2d_matrix problem """
+
+
+def rotate_2d_matrix(matrix):
+    """ n x n 2D matrix, rotate it 90 degrees clockwise """
+
+    # Transpose the matrix by swap matrix[r][c] with elements in matrix[c][r]
+    for r in range(len(matrix)):
+        for c in range(r, len(matrix)):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+    # Revese each row after transpose the given matrix.
+    for row in matrix:
+        row.reverse()
